f74e531475193a36f35b21812eb70bc93fa5a022
Sergii_Stepanov <Sergii_Stepanov@EPUAKYIW0804.kyiv.epam.com>
2015-10-20 14:48:57 +0300

    Create Product Details page

--- src/main/java/com/rozetka/pages/ProductDetailsPage.java
+++ src/main/java/com/rozetka/pages/ProductDetailsPage.java
@@ -1,7 +1,21 @@
 package com.rozetka.pages;
 
+import org.openqa.selenium.By;
+
 import com.rozetka.panels.SearchPane;
 
 public class ProductDetailsPage extends SearchPane {
+	
+	private final By productTitle=By.cssSelector("h1[class='detail-title']");
+	
+	public boolean doesProductHaveParametrs(String... parametrs){
+		boolean has=true;
+		for(String param: parametrs){
+			if (getElementBy(productTitle).getText().contains(param)){
+				has=false;
+			}
+		}
+		return has;
+	}
 
 }

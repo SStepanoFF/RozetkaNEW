af4c2a25b3bb84b6dd6d92673564a984761967ab
Sergii_Stepanov <Sergii_Stepanov@EPUAKYIW0804.kyiv.epam.com>
2015-10-20 15:22:54 +0300

    test

--- src/main/java/com/rozetka/pages/ProductDetailsPage.java
+++ src/main/java/com/rozetka/pages/ProductDetailsPage.java
@@ -7,6 +7,7 @@ import com.rozetka.panels.SearchPane;
 public class ProductDetailsPage extends SearchPane {
 	
 	private final By productTitle=By.cssSelector("h1[class='detail-title']");
+	private final By wishList=By.xpath("//div[@name='wishlists']/a");
 	
 	public boolean doesProductHaveParametrs(String... parametrs){
 		boolean has=true;
@@ -17,5 +18,7 @@ public class ProductDetailsPage extends SearchPane {
 		}
 		return has;
 	}
+	
+	//test
 
 }

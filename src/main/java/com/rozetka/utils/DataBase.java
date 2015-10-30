e3c1784126c58f36606b666eb5cd76c79f9b897b
SStepanoFF <sergiistepanov85@gmail.com>
2015-10-30 17:47:04 +0200

    refactored DataBase

--- src/main/java/com/rozetka/utils/DataBase.java
+++ src/main/java/com/rozetka/utils/DataBase.java
@@ -4,21 +4,27 @@ import java.sql.*;
 import java.text.SimpleDateFormat;
 
 public class DataBase {
+    private static final String MYSQL_DRIVER="com.mysql.jdbc.Driver";
+    private static final String ORACLE_DRIVER="org.postgresql.Driver";
+    private static final String MSSQL_DRIVER="com.microsoft.sqlserver.jdbc.SQLServerDriver";
 
-    private static final String dbLogin = PropertyLoader.loadUserProperty("dbUserName");//Loader.loadProperty("dbLogin");
-    private static final String dbPass = PropertyLoader.loadUserProperty("dbPassword");//Loader.loadProperty("dbPass");
-    private static final String dbLocation = PropertyLoader.loadUserProperty("dbLocation");/*   //mysql-kstest2.t1.tenet:3306/sutter_health      */
+    private static final String DB_LOGIN = PropertyLoader.loadUserProperty("dbUserName");
+    private static final String DB_PASS = PropertyLoader.loadUserProperty("dbPassword");
+    private static final String DB_LOCATION = PropertyLoader.loadUserProperty("dbLocation");/*   //mysql-kstest2.t1.tenet:3306/sutter_health      */
 
+    private static String dbDriver="";
+    private static String dbConnect="";
 
     public static final String executeSQLQuery(String query, String columnName){
         String result = null;
         Connection conn = null;
         Statement statement = null;
         ResultSet resultSets=null;
+        determinDB();
         try {
-            Class.forName("com.mysql.jdbc.Driver");//Driver registration
-            conn = DriverManager.getConnection("jdbc:mysql:"+dbLocation,
-                    dbLogin, dbPass);//Set connection to the database
+            Class.forName(dbDriver);//Driver registration
+            conn = DriverManager.getConnection(dbConnect+ DB_LOCATION,
+                    DB_LOGIN, DB_PASS);//Set connection to the database
             statement = conn.createStatement();//Request preparation
             resultSets = statement.executeQuery(query);
             while (resultSets.next()) {
@@ -43,6 +49,23 @@ public class DataBase {
             throw new RuntimeException("ERROR! Can't find in DB!");
         } else return result;
     }
+
+    private static void determinDB(){
+        switch (PropertyLoader.loadProperty("dbType").toLowerCase()){
+            case "mysql" :
+                dbDriver=MYSQL_DRIVER;
+                dbConnect="jdbc:mysql:";
+                break;
+            case "oracle" :
+                dbDriver=ORACLE_DRIVER;
+                dbConnect="jdbc:oracle:";
+                break;
+            case "mssql" :
+                dbDriver=MYSQL_DRIVER;
+                dbConnect="jdbc:sqlserver";
+                break;
+        }
+    }
     
 //    public static final String[][] executeSQLQueryToArray(String query){
 //    	 String result = null;
@@ -54,8 +77,8 @@ public class DataBase {
 //         
 //         try {
 //             Class.forName("com.mysql.jdbc.Driver");//Driver registration
-//             conn = DriverManager.getConnection("jdbc:mysql:"+dbLocation,
-//                     dbLogin, dbPass);//Set connection to the database
+//             conn = DriverManager.getConnection("jdbc:mysql:"+DB_LOCATION,
+//                     DB_LOGIN, DB_PASS);//Set connection to the database
 //             statement = conn.createStatement();//Request preparation
 //             resultSets = statement.executeQuery(query);
 //             while(resultSets.next()){
